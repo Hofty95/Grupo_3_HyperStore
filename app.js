78ac444f82376ecdfb977e8d29d31df06cb2da59
Thiago <thiagodelgado.dev@gmail.com>
2023-01-22 14:24:05 -0300

    add view cambio contraseña with styles responsive

--- app.js
+++ app.js
@@ -1,28 +1,43 @@
 /* Constantes Base */
 
 const express = require("express");
-const app = express()
+const app = express();
 const path = require("path");
-const port = 3030
-
+const port = 3030;
 
 /* Elementos Estaticos */
 
-app.use(express.static(path.resolve(__dirname, 'public' )));
-
+app.use(express.static(path.resolve(__dirname, "public")));
 
 /* Rutas */
 
-app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, "views", "home.html")));
-app.get("/login", (req, res) => res.sendFile(path.resolve(__dirname, "views", "login.html")));
-app.get("/register", (req, res) => res.sendFile(path.resolve(__dirname, "views", "register.html")));
-app.get("/detalle", (req, res) => res.sendFile(path.resolve(__dirname, "views", "detalle.html")));
-app.get("/carrito", (req, res) => res.sendFile(path.resolve(__dirname, "views", "carrito.html")));
-app.get("/resultado", (req, res) => res.sendFile(path.resolve(__dirname, "views", "busqueda.html")));
-app.get("/help", (req, res) => res.sendFile(path.resolve(__dirname, "views", "help.html")));
-
-
+app.get("/", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "views", "home.html"))
+);
+app.get("/login", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "views", "login.html"))
+);
+app.get("/register", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "views", "register.html"))
+);
+app.get("/detalle", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "views", "detalle.html"))
+);
+app.get("/carrito", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "views", "carrito.html"))
+);
+app.get("/resultado", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "views", "busqueda.html"))
+);
+app.get("/help", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "views", "help.html"))
+);
+app.get("/recuperarClave", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "views", "cambioContraseñal.html"))
+);
 
 /* Servidor */
 
-app.listen(port, () => console.log(`Server Running in: http://localhost:${port}`));
+app.listen(port, () =>
+  console.log(`Server Running in: http://localhost:${port}`)
+);

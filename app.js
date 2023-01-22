a822cb48c393a4d56acf1b2f7b0cbc3842b10ce1
Thiago <thiagodelgado.dev@gmail.com>
2023-01-22 14:28:01 -0300

    add route

--- app.js
+++ app.js
@@ -11,15 +11,30 @@ app.use(express.static(path.resolve(__dirname, "public")));
 
 /* Rutas */
 
-app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, "views", "home.html")));
-app.get("/login", (req, res) => res.sendFile(path.resolve(__dirname, "views", "login.html")));
-app.get("/register", (req, res) => res.sendFile(path.resolve(__dirname, "views", "register.html")));
-app.get("/detalle", (req, res) => res.sendFile(path.resolve(__dirname, "views", "detalle.html")));
-app.get("/carrito", (req, res) => res.sendFile(path.resolve(__dirname, "views", "carrito.html")));
-app.get("/resultado", (req, res) => res.sendFile(path.resolve(__dirname, "views", "busqueda.html")));
-app.get("/help", (req, res) => res.sendFile(path.resolve(__dirname, "views", "help.html")));
-app.get("/dashboard", (req, res) => res.sendFile(path.resolve(__dirname, "views", "dashboard.html")));
-
+app.get("/", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "views", "home.html"))
+);
+app.get("/login", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "views", "login.html"))
+);
+app.get("/register", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "views", "register.html"))
+);
+app.get("/detalle", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "views", "detalle.html"))
+);
+app.get("/carrito", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "views", "carrito.html"))
+);
+app.get("/resultado", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "views", "busqueda.html"))
+);
+app.get("/help", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "views", "help.html"))
+);
+app.get("/dashboard", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "views", "dashboard.html"))
+);
 
 app.get("/", (req, res) =>
   res.sendFile(path.resolve(__dirname, "views", "home.html"))
@@ -44,6 +59,9 @@ app.get("/help", (req, res) =>
 );
 app.get("/recuperarClave", (req, res) =>
   res.sendFile(path.resolve(__dirname, "views", "cambioContraseñal.html"))
+);
+app.get("/dashboard", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "views", "dashboard.html"))
 );
 
 /* Servidor */

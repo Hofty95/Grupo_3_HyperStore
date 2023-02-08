f29d6130bbf1d27c3a08cc9b734eebdaf14e0c94
victoria <hofty95@gmail.com>
2023-02-08 12:20:45 -0300

    agregamos metodo override

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const methodOverride =  require('method-override');
 
 /* Router */
 const mainRouter = require('./routes/main');
@@ -21,6 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
 
 /* Rutas */
 app.use('/', mainRouter);

67b4214f0f682d5d3071e862c8ca698299de50cc
victoria <hofty95@gmail.com>
2023-02-22 07:46:13 -0300

    agrego funcion busqueda

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,8 +5,36 @@ const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 module.exports = {
     busqueda:(req,res) => {
+        const products = readJson('products.json');
+        const categories = readJson('categories.json');
+        const {keywords, state, category} = req.query;
+
+        let productsFound = products
+
+        if(keywords){
+            productsFound = productsFound.filter(product => product.name.toLowerCase().includes(keywords.toLowerCase()) || product.description.toLowerCase().includes(keywords.toLowerCase()))
+        }
+
+        if(state){
+            if (state === 'asc') {
+                productsFound = productsFound.sort((before, after) => {
+                    return before.price - after.price
+                })
+            }else{
+                return after.price - before.price
+            }
+        }
+
+        if (category){
+            productsFound = productsFound.filter(product => {
+                product.category === category
+            })
+        }
+
         return res.render('product/busqueda',{
-            title: 'Search'
+            title: 'Search',
+            productsFound,
+            categories
         })
     },
     carrito:(req,res) => {

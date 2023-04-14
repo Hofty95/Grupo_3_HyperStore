d87d1e825defc3328dfbaf6711ab528a4f3b1b0c
gnicolaslan <gabrielnicolas.dg@gmail.com>
2023-04-14 12:16:43 -0300

    fix admin middleware

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -6,11 +6,12 @@ const checkUserAdmin = require("../middlewares/checkUserAdmin");
 const { uploadProductImages } = require("../middlewares/uploadIMG");
 const addProductValidation = require("../validations/addProductValidation");
 const editProductValidation = require("../validations/editProductValidation");
+const checkUserLogin = require("../middlewares/checkUserLogin");
 
 
 /* /Admin */
 
-router.get("/dashboard", Admin);
+router.get("/dashboard",checkUserAdmin, Admin);
 router.post("/dashboard", uploadProductImages.array('images'), addProductValidation,storeProduct)
 router.get("/dashboard/edit/:id",checkUserAdmin, editProduct)
 router.put("/dashboard/edit/:id", editProductValidation,saveEditProduct)

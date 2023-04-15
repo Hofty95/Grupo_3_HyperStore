7b8005fd424dbfe9ed77e7ee320539541582740c
gnicolaslan <gabrielnicolas.dg@gmail.com>
2023-04-14 21:08:18 -0300

    test adminController

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -113,76 +113,73 @@ module.exports = {
         })
         .catch(error => console.log(error));
     },
-    saveEditProduct : (req, res) => {
+    saveEditProduct: (req, res) => {
         const errors = validationResult(req)
-
+    
         if (errors.isEmpty()) {
-            const {id} = req.params;
-            const {name, price, discount, description, specifications, categories, gama, brand} = req.body
-
+            const { id } = req.params;
+            const { name, price, discount, description, specifications, categories, gama, brand } = req.body
+    
             db.Product.update(
-            {
-                name : name.trim(),
-                price : +price,
-                discount : +discount,
-                description : description.trim(),
-                specifications : specifications.trim(),
-                gamaId : +gama,
-                brandId : +brand
-            },
-            {
-                where : {
-                    id : id
-                }
-            }
-            )
-            .then((product) =>{
-                categories.forEach( async (category) => {
-                    await db.productCategories.update({
-                        categoryId : category
-                    },
-                    {
-                        where : {
-                            productId : product.id
-                        }
-                    })
-
-                return res.redirect("/admin/dashboard");                
-            })
-            })
-            .catch(error => console.log(error))
-
-        }else{
-
-        const {id} = req.params
-        const productToEdit = db.Product.findByPk(id,{
-            include : [
                 {
-                  association : 'categories',
-                  attributes : ['id','name']
+                    name: name.trim(),
+                    price: +price,
+                    discount: +discount,
+                    description: description.trim(),
+                    specifications: specifications.trim(),
+                    gamaId: +gama,
+                    brandId: +brand
+                },
+                {
+                    where: {
+                        id: id
+                    }
                 }
-              ]
-        });
-        const allCategories = db.Category.findAll();
-        const gamas = db.Gama.findAll();
-        const brands = db.Brand.findAll();
-
-       
-
-        Promise.all([productToEdit,allCategories,gamas,brands])
-        .then(([productToEdit,allCategories,gamas,brands])=>{
-
-         
-            return res.render("admin/dashboard_edit",{
-                title: "HyperStore | edit",
-                allCategories,
-                ...productToEdit.dataValues,
-                gamas,
-                brands
-            })
-        })
-        .catch(error => console.log(error));
-
+            )
+                .then(async (product) => {
+                    for (const category of categories) {
+                        await db.productCategories.update(
+                            {
+                                categoryId: category
+                            },
+                            {
+                                where: {
+                                    productId: product.id
+                                }
+                            }
+                        )
+                    }
+                    return res.redirect("/admin/dashboard");
+                })
+                .catch(error => console.log(error))
+    
+        } else {
+    
+            const { id } = req.params
+            const productToEdit = db.Product.findByPk(id, {
+                include: [
+                    {
+                        association: 'categories',
+                        attributes: ['id', 'name']
+                    }
+                ]
+            });
+            const allCategories = db.Category.findAll();
+            const gamas = db.Gama.findAll();
+            const brands = db.Brand.findAll();
+    
+            Promise.all([productToEdit, allCategories, gamas, brands])
+                .then(([productToEdit, allCategories, gamas, brands]) => {
+                    return res.render("admin/dashboard_edit", {
+                        title: "HyperStore | edit",
+                        allCategories,
+                        ...productToEdit.dataValues,
+                        gamas,
+                        brands
+                    })
+                })
+                .catch(error => console.log(error));
+    
         }
     }
 }

778979c0e1c31f419b953d5d72931325f79896f3
victoria <hofty95@gmail.com>
2023-04-28 12:43:23 -0300

    fix merge

--- src/routes/api/productApi.js
+++ src/routes/api/productApi.js
@@ -2,18 +2,6 @@ const express = require('express');
 /* const { confirmRemove } = require('../controllers/adminController'); */
 const router = express.Router();
 
-<<<<<<< HEAD
-const {busqueda, carrito, category, detalle, confirmRemove, remove} = require('../../controllers/api/productApiController');
-const checkUserLogin = require('../../middlewares/checkUserLogin');
-/* /api/product */
-
-router.get('/busqueda', busqueda)
-router.get('/carrito',checkUserLogin, carrito)
-router.get('/category', category)
-router.get('/detalle/:id', detalle)
-router.get('/remove/:id',checkUserLogin,confirmRemove)
-router.delete('/remove/:id', remove)
-=======
 const {busqueda, carrito, category, detalle, remove} = require('../../controllers/api/productApiController');
 const checkUserLogin = require('../../middlewares/checkUserLogin');
 /* /api/product */
@@ -23,6 +11,5 @@ router.get('/',checkUserLogin, carrito)
 router.get('/category', category)
 router.get('/:id', detalle)
 router.delete('/:id', remove)
->>>>>>> develop
 
 module.exports = router;

daa489b2648ea98d1bab20a26e255e115c4f3e1d
Thiago <thiagodelgado.dev@gmail.com>
2023-05-29 08:41:00 -0300

    finished api toggle favorites & get all favorites

--- src/controllers/api/favoritesApiController.js
+++ src/controllers/api/favoritesApiController.js
@@ -1,20 +1,26 @@
 const createResponseError = require("../../helpers/createResponseError");
 const sendSucessResponse = require("../../helpers/sendSucessResponse");
-const { getUserWithFavorites } = require("../../services/favoritesServices");
+const {
+  getUserWithFavorites,
+  addOrRemoveToFavorite,
+} = require("../../services/favoritesServices");
 
 module.exports = {
   getFavorites: async (req, res) => {
     try {
-        /* const {id}  = req.session.userLogin; */
-        const user = await getUserWithFavorites({ userId: 1 });
-        sendSucessResponse(res, { data: user });
+      const { id } = req.session.userLogin;
+      const user = await getUserWithFavorites({ userId: id });
+      sendSucessResponse(res, { data: user });
     } catch (error) {
       createResponseError(res, error);
     }
   },
   toggleProductFavorite: async (req, res) => {
     try {
-        
+      const { id } = req.session.userLogin;
+      const { productId } = req.body;
+      const user = await addOrRemoveToFavorite({ userId: id, productId });
+      sendSucessResponse(res, { data: user.productsFavorite });
     } catch (error) {
       createResponseError(res, error);
     }

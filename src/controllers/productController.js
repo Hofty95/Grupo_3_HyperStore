92297aed12a45fa569f90fb8234e478d4f18fc73
gnicolaslan <gabrielnicolas.dg@gmail.com>
2023-05-30 22:19:02 -0300

    vista allProducts creada

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -3,14 +3,14 @@ const { Op } = require("sequelize");
 const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 module.exports = {
-busqueda: async (req, res) => {
-    const { keywords, gama, brand, category} = req.query;
+  busqueda: async (req, res) => {
+    const { keywords, gama, brand, category } = req.query;
     const categories = await db.Category.findAll()
     const brands = await db.Brand.findAll()
 
 
     let productsFound = db.Product.findAll({
-      include : {
+      include: {
         association: "images",
         attributes: ["name"],
       },
@@ -18,44 +18,44 @@ busqueda: async (req, res) => {
 
     if (keywords) {
       productsFound = db.Product.findAll({
-        include : {
+        include: {
           association: "images",
           attributes: ["name"],
         },
-        where : {
+        where: {
           [Op.or]: [
-          {
-            name: {
-              [Op.substring]: `%${keywords}%`,
+            {
+              name: {
+                [Op.substring]: `%${keywords}%`,
+              },
             },
-          },
-          {
-            description: {
-              [Op.substring]: `%${keywords}%`,
-            },
-          }
-        ]
-      }
-    });
-  }
+            {
+              description: {
+                [Op.substring]: `%${keywords}%`,
+              },
+            }
+          ]
+        }
+      });
+    }
 
     if (gama) {
       productsFound = db.Product.findAll({
-        include : {
+        include: {
           association: "images",
           attributes: ["name"],
         },
-        where : {gamaId : gama}
+        where: { gamaId: gama }
       })
     }
 
     if (brand) {
       productsFound = db.Product.findAll({
-        include : {
+        include: {
           association: "images",
           attributes: ["name"],
         },
-        where : {brandId : brand}
+        where: { brandId: brand }
       })
     }
 
@@ -81,106 +81,128 @@ busqueda: async (req, res) => {
       });
     }
     Promise.all([productsFound])
-    .then(([productsFound])=>{
-      return res.render("product/busqueda", {
-        title: "Search",
-        productsFound,
-        categories,
-        numSearched: productsFound.length,
-        brands,
-        toThousand
-      }); 
-    })
+      .then(([productsFound]) => {
+        return res.render("product/busqueda", {
+          title: "Search",
+          productsFound,
+          categories,
+          numSearched: productsFound.length,
+          brands,
+          toThousand
+        });
+      })
+  },
+  showAllProducts: async (req, res) => {
+    const categories = db.Category.findAll();
+    const brands = db.Brand.findAll();
+
+    let allProducts = db.Product.findAll({
+      include: {
+        association: "images",
+        attributes: ["name"],
+      },
+    });
+
+    Promise.all([allProducts, categories, brands])
+      .then(([allProducts, categories, brands]) => {
+        return res.render("product/allProducts", {
+          title: "Productos",
+          allProducts,
+          categories,
+          brands,
+          toThousand
+        });
+      })
   },
   carrito: (req, res) => {
-    const categories =  db.Category.findAll();
-    const brands =  db.Brand.findAll();
+    const categories = db.Category.findAll();
+    const brands = db.Brand.findAll();
 
-    Promise.all([categories,brands])
-    .then(([categories,brands])=>{
+    Promise.all([categories, brands])
+      .then(([categories, brands]) => {
         return res.render("product/carrito", {
-             title : "Hyper Store | Carrito",
-            categories,
-            brands,
-        });            
-    })
+          title: "Hyper Store | Carrito",
+          categories,
+          brands,
+        });
+      })
   },
   category: (req, res) => {
-    const categories =  db.Category.findAll();
-    const brands =  db.Brand.findAll();
+    const categories = db.Category.findAll();
+    const brands = db.Brand.findAll();
 
-    Promise.all([categories,brands])
-    .then(([categories,brands])=>{
+    Promise.all([categories, brands])
+      .then(([categories, brands]) => {
         return res.render("product/category", {
-            title: "Hyper Store | Category",
-            categories,
-            brands,
+          title: "Hyper Store | Category",
+          categories,
+          brands,
         });
-    })
+      })
   },
   detalle: (req, res) => {
     const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-    const categories =  db.Category.findAll();
-    const brands =  db.Brand.findAll();
+    const categories = db.Category.findAll();
+    const brands = db.Brand.findAll();
 
-    const product = db.Product.findByPk(req.params.id,{
-      include : [
+    const product = db.Product.findByPk(req.params.id, {
+      include: [
         {
-          association : 'images',
-          attributes : ['name']
+          association: 'images',
+          attributes: ['name']
         }
       ]
     })
-    
+
 
     const ofertProducts = db.Product.findAll({
-      include : [
+      include: [
         {
-          association : 'images',
-          attributes : ['name']
+          association: 'images',
+          attributes: ['name']
         }
       ],
-      limit : 6,
+      limit: 6,
       where: {
         discount: {
           [Op.ne]: 25,
         },
       },
     })
-    Promise.all([product,ofertProducts,categories,brands])
-    .then(([product,ofertProducts,categories,brands]) =>{
-      return res.render('product/detalle',{
-        title : 'Hyper Store | Detalle de producto',
-        ...product.dataValues,
-        toThousand,
-        ofertProducts,
-        categories,
-        brands
+    Promise.all([product, ofertProducts, categories, brands])
+      .then(([product, ofertProducts, categories, brands]) => {
+        return res.render('product/detalle', {
+          title: 'Hyper Store | Detalle de producto',
+          ...product.dataValues,
+          toThousand,
+          ofertProducts,
+          categories,
+          brands
+        })
       })
-    }) 
   },
   confirmRemove: (req, res) => {
     const id = req.params.id;
     const product = db.Product.findByPk(id)
-    const categories =  db.Category.findAll();
-    const brands =  db.Brand.findAll();
+    const categories = db.Category.findAll();
+    const brands = db.Brand.findAll();
 
     Promise.all(product)
-    .then((product) => {
-      return res.render("product/confirmRemove", {
-        title: "HyperStore | remove",
-        ...product.dataValues,
-        categories,
-        brands
-      });
-    })
+      .then((product) => {
+        return res.render("product/confirmRemove", {
+          title: "HyperStore | remove",
+          ...product.dataValues,
+          categories,
+          brands
+        });
+      })
   },
   remove: (req, res) => {
-    const {id} = req.params
-    db.productCategories.destroy({where : {productId : id}})
-    .then(async () => {
-      await db.Product.destroy({where: {id : id}})
-      return res.redirect(`/admin/dashboard`);
-    })
+    const { id } = req.params
+    db.productCategories.destroy({ where: { productId: id } })
+      .then(async () => {
+        await db.Product.destroy({ where: { id: id } })
+        return res.redirect(`/admin/dashboard`);
+      })
   },
 };
--- src/routes/product.js
+++ src/routes/product.js
@@ -2,11 +2,12 @@ const express = require('express');
 /* const { confirmRemove } = require('../controllers/adminController'); */
 const router = express.Router();
 
-const {busqueda, carrito, category, detalle, confirmRemove, remove} = require('../controllers/productController');
+const {busqueda, carrito, category, detalle, confirmRemove, remove, showAllProducts} = require('../controllers/productController');
 const checkUserLogin = require('../middlewares/checkUserLogin');
 /* product */
 
 router.get('/busqueda', busqueda)
+router.get('/todos', showAllProducts)
 router.get('/carrito',checkUserLogin, carrito)
 router.get('/category', category)
 router.get('/detalle/:id', detalle)

23fec3a6459c99e2d0dfe2f122abb6626f7dc561
Thiago <thiagodelgado.dev@gmail.com>
2023-06-12 12:29:19 -0300

    config deploy

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -10,10 +10,11 @@ module.exports = {
     port :  process.env.DB_PORT
   },
   test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
+    username: process.env.MYSQL_ADDON_USER,
+    password: process.env.MYSQL_ADDON_PASSWORD,
+    database: process.env.MYSQL_ADDON_DB,
+    host: process.env.MYSQL_ADDON_HOST,
+    port : process.env.MYSQL_ADDON_PORT,
     dialect: "mysql",
   },
   production: {
